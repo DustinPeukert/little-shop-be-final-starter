f035d6d910d9e2e80d1a71fa7746b6d031561685
Dustin Peukert <dpeukert04@gmail.com>
2025-01-20 16:23:28 -0500

    add new index params

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::MerchantsController < ApplicationController
     end
 
     include_count = params[:count].present? && params[:count] == "true"
-    render json: MerchantSerializer.new(merchants, { params: { count: include_count }})
+    render json: MerchantSerializer.new(merchants, { params: { count: include_count, coupon_count: true, invoice_coupon_count: true }})
   end
 
   def show
